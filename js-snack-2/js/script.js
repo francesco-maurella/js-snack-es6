c6066fca2d0d882ce06562680acc9ab48d667b68
Francesco Maurella <francesco.maurella@live.it>
2021-01-19 16:38:49 +0100

    Up

--- js-snack-2/js/script.js
+++ js-snack-2/js/script.js
@@ -1,13 +1,8 @@
 /*
-Creare un array di oggetti di squadre di calcio.
-
-Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
-Nome sarà l’unica proprietà da compilare,
-le altre saranno tutte settate a 0.
-Generare numeri random al posto degli 0 nelle proprietà:
-Punti fatti e falli subiti
-Infine usando la destrutturazione creiamo un nuovo array i cui elementi
-contengono solo nomi e falli subiti e stampiamo tutto in console.
+Array di oggetti di squadre di calcio.
+Proprietà di ogni oggetto: nome, punti fatti, falli subiti.
+Punti e falli sono settati a 0 e verranno riempiti con numeri random.
+Stampare array contenene nomi squadre e falli subiti.
 */
 
 // funzione generatrice numeri random
