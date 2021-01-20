a72a48be76d490e18d9e4a4fafc7d527a3676e4b
Francesco Maurella <francesco.maurella@live.it>
2021-01-20 16:25:26 +0100

    Function in ecma5 (snack3)

--- 3/ecmascript5/js/script.js
+++ 3/ecmascript5/js/script.js
@@ -5,11 +5,20 @@ La funzione ritornerà un nuovo array con i valori
 che hanno la posizione compresa tra i due numeri
 */
 
+// funzione elementi filtrati in nuovo array
+function getFilteredArray(array, min, max) {
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    // se gli elementi del container sono compresi tra posizione "min" e "max"
+    if (i >= (min-1) && i <= (max-1)) {
+      newArray.push(array[i]) // duplichiamoli nel nuovo array
+    }
+  }
+  return newArray;
+}
+
 // creiamo un primo contenitore
 var container = ['A','B','C','D','E','F','G','H','I','L','M','N','O'];
-
-// dichiariamo un secondo contenitore
-var newContainer = [];
 
 // valore "x"
 var x = 5;
@@ -17,12 +26,8 @@ var x = 5;
 // valore "y"
 var y = 10;
 
-// duplichiamo gli elementi del container compresi tra posizione "x" e "y"
-for (var i = 0; i < container.length; i++) {
-  if (i >= (x-1) && i <= (y-1)) {
-    newContainer.push(container[i])
-  }
-}
+// creiamo un nuovo array contenente gli elementi del contailer filtrati
+var newContainer = getFilteredArray(container, x, y);
 
 // stampiamo il secondo container
 console.log(newContainer);
--- 3/js/script.js
+++ 3/js/script.js
@@ -8,16 +8,17 @@ che hanno la posizione compresa tra i due numeri
 // creiamo un primo contenitore
 const container = ['A','B','C','D','E','F','G','H','I','L','M','N','O'];
 
-
 // valore "x"
 let x = 5;
 
 // valore "y"
 let y = 10;
 
+// riportiamo gli elementi del container compresi tra posizione "x" e "y"
+// in un nuovo contenitore
 const newContainer = container.filter ((element, index, array) => {
   return index >= (x-1) && index <= (y-1);
 })
 
-// stampiamo il secondo container
+// stampiamo il nuovo container
 console.log(newContainer);
